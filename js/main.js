cfa3d2d33cf84a200da856fb38d218ae7ba7e427
bryan kennedy <bkennedy@smm.org>
2014-06-08 12:20:19 -0500

    Improving some video player settings

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-// Mute the awful music while in development
-videojs("example_video_1", { muted: true });
+/**
+ * Video player settings
+ *
+ * Mute any music
+ * Any audio comes from the SOS.
+ * This will help demonstrate any problems caused by switching on the amp.
+ *
+ * Disable the preloading spinner icon.
+ * We don't want to show this to museum visitors while the video is loading.
+ */
+videojs("example_video_1", {
+    muted: true,
+    children: {
+        loadingSpinner: false
+    }
+});
