7db0107d1e20b5443d9a7c9d1d0a5639f22e382e
bryan kennedy <bkennedy@smm.org>
2014-06-09 16:17:04 -0500

    Fixing variable name

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var server = http.createServer(function (req, res) {
             if (err) {
                 console.error('Error serving %s - %s', req.url, err.message);
                 if (err.status === 404 || err.status === 500) {
-                    file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+                    fileServer.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
                 } else {
                     res.writeHead(err.status, err.headers);
                 res.end();
