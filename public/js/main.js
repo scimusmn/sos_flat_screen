8ef5d62fe6e1ec3b010d3d969f154b615cd51030
bryan kennedy <bkennedy@smm.org>
2014-06-17 15:45:27 -0500

    Adding a pie timer instead of a seconds countdown
    This is some existing code that I have. And it's also a bit more visual

--- public/js/main.js
+++ public/js/main.js
@@ -86,21 +86,70 @@ $(document).keydown(function(e){
      */
     if (e.keyCode == 82) {
         console.log('R pressed');
-        $.deck('go', 'interlude-slide')
+        $.deck('go', 'interlude-slide');
+
+        /**
+         * Start the timer
+         */
+        console.log("Starting the timer");
+        $('.next-timer').pietimer({
+            seconds: 15,
+            colour: '#B32037'
+        }, function() {
+            $.deck('go', 'black-slide')
+        });
     }
 });
 
-/**
- * Create a timer on the interlude slide
- */
-$(document).ready(function() {
-    var timerSeconds = 30
-    var submitCountdown = new Countdown(timerSeconds, function(seconds) {
-        $('.next-timer').text(seconds + ' seconds');
-    }, function() {
-        $.deck('go', 'black-slide')
-    });
-});
+jQuery.fn.pietimer = function( options, callback ) {
+    var settings = {
+        'seconds': 5,
+        'colour': 'rgba(255, 255, 255, 0.8)',
+        'height': this.height(),
+        'width': this.width()
+    };
+    if ( options ) {
+        $.extend( settings, options );
+    }
+    this.html('<canvas id="pie_timer" width="'+settings.height+'" height="'+settings.height+'"></canvas>');
+    var val = 360;
+    interval = setInterval(timer, 40);
+    function timer(){
+        var canvas = document.getElementById('pie_timer');
+        if (canvas.getContext){
+            val -= ( 360 / settings.seconds ) / 24;
+            if ( val <= 0 ){
+                clearInterval(interval);
+                canvas.width = canvas.width;
+                if(typeof callback == 'function'){
+                    callback.call();
+                }
+            } else {
+                canvas.width = canvas.width;
+                var ctx = canvas.getContext('2d');
+                var canvas_size = [canvas.width, canvas.height];
+                var radius = Math.min(canvas_size[0], canvas_size[1]) / 2;
+                var center = [canvas_size[0]/2, canvas_size[1]/2];
+                ctx.beginPath();
+                ctx.moveTo(center[0], center[1]);
+                var start = ( 3 * Math.PI ) / 2;
+                ctx.arc(
+                    center[0],
+                    center[1],
+                    radius,
+                        start - val * ( Math.PI / 180 ),
+                    start,
+                    false
+                );
+                ctx.closePath();
+                ctx.fillStyle = settings.colour;
+                ctx.fill();
+            }
+        }
+    }
+    return this;
+}
+
 
 //var iosocket = io.connect();
 
